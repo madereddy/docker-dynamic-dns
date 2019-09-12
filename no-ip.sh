fe2d0ab0f3a39a8fa3debf598f96d298b34e0809
CushItRealGood <cushenberry.adrian@gmail.com>
2019-09-12 00:49:23 +0000

    Support for Multiple Domains
    Added DOMAINS as an optional environment variable. If using multiple domains you must add "1" to the USERNAME, PASSWORD, and HOSTNAME variables and increment by 1.

--- no-ip.sh
+++ no-ip.sh
@@ -1,19 +1,64 @@
 #!/bin/bash
+if [ -z "$DOMAINS" ]
+then
+	echo "Number of Domains was not set."
+else
+        if [[ $DOMAINS != [0-9]* ]]
+        then
+		echo "Domains is not an integer."
+		exit 40 #made up exit code values here and below, not sure what exit codes should be
+	else
+		echo "Number of Domains is set to $DOMAINS"
+	fi
+fi
 
-if [ -z "$USER" ]
+for ((COUNTER=1; COUNTER<=DOMAINS; COUNTER++))
+do
+	if [[ -z $(eval echo \$$(echo "USER$COUNTER")) ]]
+	then
+		echo "No user was set for "USER$COUNTER"."
+		exit 41 #disabled for now to make testing easier
+	else
+		eval echo \$$(echo "USER$COUNTER") set as USER$COUNTER
+	fi
+done
+
+for ((COUNTER=1; COUNTER<=DOMAINS; COUNTER++))
+do 
+	if [[ -z $(eval echo \$$(echo "PASSWORD$COUNTER")) ]]
+	then
+		echo "No password was set for "PASSWORD$COUNTER"."
+		exit 42 #disabled for now to make testing easier
+	else
+		eval echo \$$(echo "PASSWORD$COUNTER") set as PASSWORD$COUNTER
+	fi
+done
+
+for ((COUNTER=1; COUNTER<=DOMAINS; COUNTER++))
+do
+	if [[ -z $(eval echo \$$(echo "HOSTNAME$COUNTER")) ]]
+	then
+		echo "No hostname was set for "HOSTNAME$COUNTER"."
+		exit 43 #disabled for now to make testing easier
+	else 
+		eval echo \$$(echo "HOSTNAME$COUNTER") set as HOSTNAME$COUNTER
+	fi
+done
+
+if [ -z "$USER" ] && [ -z "$USER1" ]
 then
 	echo "No user was set. Use -u=username"
 	exit 10
 fi
 
-if [ -z "$PASSWORD" ]
+if [ -z "$PASSWORD" ] && [ -z "$PASSWORD1" ]
 then
 	echo "No password was set. Use -p=password"
 	exit 20
 fi
 
 
-if [ -z "$HOSTNAME" ]
+if [ -z "$HOSTNAME" ] && [ -z "$HOSTNAME1" ]
 then
 	echo "No host name. Use -h=host.example.com"
 	exit 30
@@ -64,51 +109,77 @@ case "$SERVICE" in
 
 esac
 
-USERAGENT="--user-agent=\"no-ip shell script/1.0 mail@mail.com\""
-BASE64AUTH=$(echo '"$USER:$PASSWORD"' | base64)
-AUTHHEADER="--header=\"Authorization: $BASE64AUTH\""
-
-NOIPURL="https://$USER:$PASSWORD@$SERVICEURL"
-
-
-if [ -n "$IP" ] || [ -n "$HOSTNAME" ]
+if [ -z "$DOMAINS" ]
 then
-	NOIPURL="$NOIPURL?"
-fi
+    echo "Doing standard operations"
+	USERAGENT="--user-agent=\"no-ip shell script/1.0 mail@mail.com\""
+	BASE64AUTH=$(echo '"$USER:$PASSWORD"' | base64)
+	AUTHHEADER="--header=\"Authorization: $BASE64AUTH\""
+	NOIPURL="https://$USER:$PASSWORD@$SERVICEURL"
 
-if [ -n "$HOSTNAME" ]
-then
-	NOIPURL="${NOIPURL}hostname=${HOSTNAME}"
-fi
 
-if [ -n "$IP" ]
-then
+	if [ -n "$IP" ] || [ -n "$HOSTNAME" ]
+	then
+		NOIPURL="$NOIPURL?"
+	fi
+
 	if [ -n "$HOSTNAME" ]
 	then
-		NOIPURL="$NOIPURL&"
+		NOIPURL="${NOIPURL}hostname=${HOSTNAME}"
 	fi
-	NOIPURL="${NOIPURL}myip=$IP"
-fi
 
-
-echo "$AUTHHEADER $USERAGENT $NOIPURL"
-
-while :
-do
-
-	RESULT=$(wget --no-check-certificate -qO- $AUTHHEADER $USERAGENT $NOIPURL)
-
-
-	echo $RESULT
-
-
-	if [ $INTERVAL -eq 0 ]
+	if [ -n "$IP" ]
 	then
-		break
-	else
-		sleep "${INTERVAL}m"
+		if [ -n "$HOSTNAME" ]
+		then
+			NOIPURL="$NOIPURL&"
+		fi
+		NOIPURL="${NOIPURL}myip=$IP"
 	fi
 
-done
 
+	echo "$AUTHHEADER $USERAGENT $NOIPURL"
+
+	while :
+	do
+
+		RESULT=$(wget --no-check-certificate -qO- $AUTHHEADER $USERAGENT $NOIPURL)
+
+
+		echo $RESULT
+
+
+		if [ $INTERVAL -eq 0 ]
+		then
+			break
+		else
+			sleep "${INTERVAL}m"
+		fi
+
+	done
+
+else
+	while :
+	do
+		for ((COUNTER=1; COUNTER<=DOMAINS; COUNTER++))
+		do
+			if [[ -z $(eval echo \$$(echo "USER$COUNTER")) && -z $(eval echo \$$(echo "PASSWORD$COUNTER")) ]]
+			then
+				echo "An error occured for "USER$COUNTER" or "PASSWORD$COUNTER"."
+				exit 44 #disabled for now to make testing easier
+			else
+				#eval echo NOIPURL$COUNTER=https\://\$$(echo "USER$COUNTER\:\$$(echo PASSWORD$COUNTER)@$SERVICEURL")'\?'hostname=\$$(echo "HOSTNAME$COUNTER")'\&'myip=\$$(echo "IP") #show the variables getting set
+				eval NOIPURL$COUNTER=https\://\$$(echo "USER$COUNTER\:\$$(echo PASSWORD$COUNTER)@$SERVICEURL")'\?'hostname=\$$(echo "HOSTNAME$COUNTER")'\&'myip=\$$(echo "IP") #set NOIPURLs
+				eval echo RESULT$COUNTER='\$\('wget --no-check-certificate -qO- \$$(echo "NOIPURL$COUNTER")'\)'
+				eval wget --no-check-certificate -qO- \$$(echo "NOIPURL$COUNTER")
+			fi
+		done
+		if [ $INTERVAL -eq 0 ]
+		then
+			break
+		else
+			sleep "${INTERVAL}m"
+		fi
+	done
+fi
 exit 0
